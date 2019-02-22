dc4f2b4195e826307ad3f6c9333b1bcea6ad557a
Joe Walsh <joewalsh@users.noreply.github.com>
2019-02-22 15:17:05 -0500

    use specific log

--- WMF Framework/Logging.swift
+++ WMF Framework/Logging.swift
@@ -7,28 +7,40 @@ public extension Error {
     }
 }
 
+public extension OSLog {
+    public struct Subsystem {
+        static let bundle: String = Bundle.main.bundleIdentifier ?? "com.example"
+    }
+    
+    public struct Category {
+        static let general: String = "general"
+    }
+    
+    static let general = OSLog(subsystem: Subsystem.bundle, category: Category.general)
+}
+
 public func DDLogVerbose(_ message: StaticString, _ args: CVarArg...) {
     #if DEBUG
-    os_log(message, args)
+    os_log(message, log: OSLog.general, type: .debug, args)
     #endif
 }
 
 public func DDLogDebug(_ message: StaticString, _ args: CVarArg...) {
     #if DEBUG
-    os_log(message, args)
+    os_log(message, log: OSLog.general, type: .debug, args)
     #endif
 }
 
 public func DDLogInfo(_ message: StaticString, _ args: CVarArg...) {
     #if DEBUG
-    os_log(message, args)
+    os_log(message, log: OSLog.general, type: .info, args)
     #endif
 }
 
 public func DDLogWarning(_ message: StaticString, _ args: CVarArg...) {
-    os_log(message, args)
+    os_log(message, log: OSLog.general, type: .info, args)
 }
 
 public func DDLogError(_ message: StaticString, _ args: CVarArg...) {
-    os_log(message, args)
+    os_log(message, log: OSLog.general, type: .error, args)
 }
