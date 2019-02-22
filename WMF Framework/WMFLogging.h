a9ac0378facb17a564183f5ed2558a84f6e1d622
Joe Walsh <joewalsh@users.noreply.github.com>
2019-02-22 16:59:14 -0500

    compile out verbose, debug, and info for release builds

--- WMF Framework/WMFLogging.h
+++ WMF Framework/WMFLogging.h
@@ -3,9 +3,16 @@
 
 #import <os/log.h>
 
+#if DEBUG
 #define DDLogVerbose(...) os_log_debug(OS_LOG_DEFAULT, __VA_ARGS__)
 #define DDLogDebug(...) os_log_debug(OS_LOG_DEFAULT, __VA_ARGS__)
 #define DDLogInfo(...) os_log_info(OS_LOG_DEFAULT, __VA_ARGS__)
+#else
+#define DDLogVerbose(...)
+#define DDLogDebug(...)
+#define DDLogInfo(...)
+#endif
+
 #define DDLogWarn(...) os_log_info(OS_LOG_DEFAULT, __VA_ARGS__)
 #define DDLogError(...) os_log_error(OS_LOG_DEFAULT, __VA_ARGS__)
 
